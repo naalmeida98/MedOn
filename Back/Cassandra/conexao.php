04be58655f100144849eb849717cf4be6ac08a4f
Aline Dias <martinsaline558@gmail.com>
2021-11-18 21:13:51 -0300

    biblioteca de conexao cassandra

--- Back/Cassandra/conexao.php
+++ Back/Cassandra/conexao.php
@@ -4,36 +4,33 @@ require_once '../../../../MedOn/vendor/autoload.php';
 
 class Conexao
 {
-	public function conectar()
-	{
+	//public function conectar()
+	//{
 		try {
 
 			// $cluster = Cassandra::cluster()->withCredentials("cassandra", "cassandra")->withContactPoints('localhost')->withPort(9042)->build();
-			$cluster = Cassandra::cluster()->withCredentials("cassandra", "cassandra")->build();
-
-
-			$keyspace = 'medon';
+			 
 
 			// creating session with cassandra scope by keyspace
-			$session = $cluster->connect($keyspace);
+			//$session = $cluster->connect($keyspace);
 
 			// if (!$session) {
 			// 	echo "Error - Unable to connect to database";
 			// }
 
-			$statement = new Cassandra\SimpleStatement(       // also supports prepared and batch statements
-				'SELECT * FROM medon.usuario'
-			);
-			$future    = $session->executeAsync($statement);  // fully asynchronous and easy parallel execution
-			$result    = $future->get();                      // wait for the result, with an optional timeout
+			//$statement = new Cassandra\SimpleStatement(       // also supports prepared and batch statements
+			//	'SELECT * FROM medon.usuario'
+			//);
+			//$future    = $session->executeAsync($statement);  // fully asynchronous and easy parallel execution
+			//$result    = $future->get();                      // wait for the result, with an optional timeout
 
-			echo $result;
+			//echo $result;
 
 			// foreach ($result as $row) {                       // results and rows implement Iterator, Countable and ArrayAccess
 			// 	printf("The keyspace %s has a table called %s\n", $row['keyspace_name'], $row['columnfamily_name']);
 			// }
-		} catch (Exception $e) {
-			echo $e;
-		}
-	}
+		//} catch (Exception $e) {
+		//	echo $e;
+		//}
+	//}
 }
